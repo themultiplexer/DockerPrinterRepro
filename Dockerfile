e3219495c9505f54aae588d8e7cbda0611c6cda9
themultiplexer <joshua@rutschmann.tech>
2019-05-29 14:21:44 +0200

    Initial commit.

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+﻿FROM mcr.microsoft.com/windows:1809
+# IMPORTANT! Build the PrintTest solution before creating the image 
+COPY PrintTest/bin/Release/ /
+CMD [ "powershell" ]
---
+++ PrintTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace PrintTest
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            PrintDocument printDocument1 = new PrintDocument();
+            printDocument1.PrintPage += PrintDocument1_PrintPage;
+            printDocument1.Print();
+
+        }
+
+        private static void PrintDocument1_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            e.Graphics.DrawString("Hello World", new Font("Arial", 12), Brushes.Black,e.MarginBounds, StringFormat.GenericTypographic);
+            e.HasMorePages = false;
+        }
+    }
+}
